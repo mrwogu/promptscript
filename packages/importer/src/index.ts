38608dc1f94beb17083f24e18652f18085d94ccf
mrwogu <mail@wogu.pl>
2026-03-16 21:14:08 +0100

    feat(importer): export public API from index

--- packages/importer/src/index.ts
+++ packages/importer/src/index.ts
@@ -6,3 +6,13 @@
  *
  * @packageDocumentation
  */
+
+export { importFile } from './importer.js';
+export type { ImportOptions, ImportResult } from './importer.js';
+export type { DetectedFormat } from './detector.js';
+export { detectFormat, getParser } from './detector.js';
+export { classifyConfidence, ConfidenceLevel } from './confidence.js';
+export type { ScoredSection } from './confidence.js';
+export { mapSections } from './mapper.js';
+export { emitPrs } from './emitter.js';
+export type { EmitOptions } from './emitter.js';
